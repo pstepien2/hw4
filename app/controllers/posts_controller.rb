fa499006eaca7abe394b0bb72571a21dc99b47d9
Patrick Stepien <patrick.stepien@kellogg.northwestern.edu>
2023-06-04 03:46:31 +0000

    image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
     @post = Post.new
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
+    @post.picture.attach(params["post"]["picture"])
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
     @post["user_id"] = @current_user["id"]
